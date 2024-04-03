ed0d5e0512d52205787cc387fa80697a9a375195
haruueel <haruueel@gmail.com>
2024-04-03 16:36:00 +0200

    fix: table in spotify view

--- src/views/PlayListView.vue
+++ src/views/PlayListView.vue
@@ -1,51 +1,115 @@
 <template>
-    <div class="scroll-main">
-    <div v-if="playListData">
-        
-<div class="flex flex-col gap-3">
-  <PlayListHeader/>
- <div>
-  <PlayIcon class="h-12 w-12 p-3 pr-2 bg-green-500 rounded-full cursor-pointer hover:bg-green-300"/>
- </div>
- <div class="flex justify-between text-xs">
-    <div class="flex gap-6">
-    <span>#</span>
-    <span>Titulo</span>
+  <div class="scroll-main overflow-auto" id="playlist-container">
+    <div class="flex flex-col gap-3 h-full" v-if="playListData">
+      <PlayListHeader />
+      <div>
+        <PlayIcon
+          class="h-12 w-12 p-3 pr-2 bg-green-500 rounded-full cursor-pointer hover:bg-green-300"
+        />
+      </div>
+      <div class="flex flex-col w-full divide-y divide-font/20 h-full">
+        <div class="flex items-center justify-between">
+          <div
+            v-for="header in tableHeaders"
+            class="py-2"
+            :class="{
+              'w-12': header.width == 'mini',
+              'flex-1': header.width == 'normal',
+            }"
+          >
+            <span v-if="header.text != null" class="text-sm">
+              {{ header.text }}</span
+            >
+            <component :is="header.icon" v-else class="fill-font h-4" />
+          </div>
+        </div>
+        <div class="w-full h-full flex flex-col py-3">
+          <div class="flex items-center justify-between" v-for="i in 100">
+            <div class="w-12">1</div>
+            <div class="flex-1">a</div>
+            <div class="flex-1">b</div>
+            <div class="flex-1">c</div>
+            <div class="w-12">1</div>
+            <div class="w-12"></div>
+          </div>
+        </div>
+
+        <!-- <div class="flex justify-between text-xs">
+          <div class="flex gap-6">
+            <span>#</span>
+            <span>Titulo</span>
+          </div>
+          <span>Álbum</span>
+          <span>Fecha en la que se añadió</span>
+          <span class="popper" data-tooltip="Duración">
+            <ClockIcon class="h-4 fill-font -ml-7"
+          /></span>
+        </div>
+        <hr />
+        <div class="flex flex-col">
+          <PlayListSongs
+            class="hover:bg-piece p-2 coursor-pointer"
+            v-for="song in playListData.data.songs"
+            :key="song.key"
+            :title="song.title"
+            :cover="song.album.cover"
+            :album="song.album.title"
+            :author="song.authors.name"
+            :duration="song.duration"
+            :date="song.added_on"
+          />
+        </div> -->
+      </div>
     </div>
-    <span>Álbum</span>
-    <span>Fecha en la que se añadió</span>
-    <span class="popper" data-tooltip="Duración"> <ClockIcon class="h-4 fill-font -ml-7"/></span>
-</div>
-<hr>
-<div class="flex flex-col">
- <PlayListSongs
- class="hover:bg-piece p-2 coursor-pointer"
- v-for="song in playListData.data.songs"
-          :key="song.key"
-          :title="song.title"
-          :cover="song.album.cover"
-          :album="song.album.title"
-          :author="song.authors.name"
-          :duration="song.duration"
-          :date="song.added_on"
- />
-</div>
-</div>
-</div>
-</div>
+  </div>
 </template>
 <script setup>
-import axios from 'axios';
-import { ref, } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import ClockIcon from '/src/components/iconics/ClockIcon.vue';
+import axios from "axios";
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import ClockIcon from "/src/components/iconics/ClockIcon.vue";
 import PlayIcon from "/src/components/iconics/PlayIcon.vue";
 import PlayListHeader from "./PlayListView/PlayListHeader.vue";
 import PlayListSongs from "./PlayListView/PlayListSongs.vue";
-const route = useRoute()
-const router = useRouter()
-const playListData = ref(null)
-axios.get(`/src/api/endpoints/playlists/${route.params.id}.json`).then ((response)=> {
-    playListData.value = response.data
-});
+const route = useRoute();
+const router = useRouter();
+const playListData = ref(null);
+axios
+  .get(`/src/api/endpoints/playlists/${route.params.id}.json`)
+  .then((response) => {
+    playListData.value = response.data;
+  });
+
+const tableHeaders = [
+  {
+    text: "#",
+    icon: null,
+    width: "mini",
+  },
+  {
+    text: "Título",
+    icon: null,
+    width: "normal",
+  },
+  {
+    text: "Álbum",
+    icon: null,
+    width: "normal",
+  },
+  {
+    text: "Fecha en la que se añadió",
+    icon: null,
+    width: "normal",
+  },
+  {
+    text: null,
+    icon: ClockIcon,
+    width: "mini",
+  },
+  {
+    text: "",
+    icon: null,
+    width: "mini",
+  },
+];
 </script>
--- src/views/PlayListView/PlayListHeader.vue
+++ src/views/PlayListView/PlayListHeader.vue
@@ -1,50 +1,64 @@
 <template>
-    <div v-if="playListData">
+  <div v-if="playListData">
     <div class="flex p-5 items-center h-[250px] w-full text-heading relative">
-    <div class="h-[200px] w-[200px]">
-        <img class="h-full w-full rounded" :src="playListData.data.cover" alt="imagen">
+      <div class="h-[200px] w-[200px]">
+        <img
+          class="h-full w-full rounded"
+          :src="playListData.data.cover"
+          alt="imagen"
+        />
+      </div>
+      <div class="flex flex-col gap-6 absolute bottom-6 left-60">
+        <span class="text-xs">Playlist</span>
+        <span class="text-7xl font-bold tracking-tighter">{{
+          playListData.data.name
+        }}</span>
+        <div class="flex gap-1 text-xs">
+          <span
+            v-for="(author, index) in playListData.data.authors"
+            :key="index"
+            class="hover:underline cursor-pointer"
+            >{{ author.name }} ·</span
+          >
+          <span>7 ME GUSTA ·</span>
+          <span>CANCIONES,</span>
+          <span
+            >{{
+              minutesToString(playListData.data.total_duration)
+            }}
+            aproximadamente</span
+          >
+        </div>
+      </div>
     </div>
-    <div class="flex flex-col gap-6 absolute bottom-6 left-60">
-    <span class="text-xs">Playlist</span>
-    <span class="text-7xl">{{  playListData.data.name }}</span>
-    <div class="flex gap-1 text-xs">
-        <span 
-        v-for="(author, index) in playListData.data.authors"
-        :key="index"
-        class=" hover:underline cursor-pointer">{{author.name}} ·</span>
-        <span>7 ME GUSTA ·</span>
-        <span>CANCIONES,</span>
-        <span>{{ minutesToString(playListData.data.total_duration)}} aproximadamente</span>
-        
-    </div>
-    </div>
-</div>
-</div>
+  </div>
 </template>
 
 <script setup>
-import axios from 'axios';
-import { ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-const route = useRoute()
-const playListData = ref(null)
-axios.get(`/src/api/endpoints/playlists/${route.params.id}.json`).then ((response)=> {
-    playListData.value = response.data
-});
+import axios from "axios";
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+const route = useRoute();
+const playListData = ref(null);
+axios
+  .get(`/src/api/endpoints/playlists/${route.params.id}.json`)
+  .then((response) => {
+    playListData.value = response.data;
+  });
 function minutesToString(minutes) {
   var hour = Math.floor(minutes / 60);
   var minute = minutes % 60;
-  
-  var result = '';
-  
+
+  var result = "";
+
   if (hour > 0) {
-    result += hour + ' h ';
+    result += hour + " h ";
   }
-  
+
   if (minute > 0 || hour > 0) {
-    result += minute + ' min';
+    result += minute + " min";
   }
-  
+
   return result.trim();
 }
 </script>
